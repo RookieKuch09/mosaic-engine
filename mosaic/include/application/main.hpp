cb291f88551542ff2e0f07b7b13efc5a132c0503
RookieKuch09 <rookiekuch09@gmail.com>
2025-05-08 19:47:50 +0930

    Rollback on windows potential support

--- mosaic/include/application/main.hpp
+++ mosaic/include/application/main.hpp
@@ -2,7 +2,6 @@
 
 #include "application.hpp"
 
-#if defined(MACOS) || defined(LINUX)
 std::int32_t main()
 {
     Mosaic::Application* application = Mosaic::CreateApplication();
@@ -11,18 +10,3 @@ std::int32_t main()
 
     return application->Run();
 }
-#elif defined(WINDOWS)
-std::int32_t wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
-{
-    FILE* fp;
-    freopen_s(&fp, "CONOUT$", "w", stdout);
-    freopen_s(&fp, "CONOUT$", "w", stderr);
-    freopen_s(&fp, "CONIN$", "r", stdin);
-
-    Mosaic::Application* application = Mosaic::CreateApplication();
-
-    application->Setup();
-
-    return application->Run();
-}
-#endif
